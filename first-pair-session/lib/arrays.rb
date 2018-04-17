d20c8663ab03e9c1bbae13e9d36aaf8ff64d421f
Ellie Keen <elliekeen95@gmail.com>
2018-04-17 09:29:56 +0100

    Completed arrays exercise + removed spec

---
+++ first-pair-session/lib/arrays.rb
@@ -0,0 +1,21 @@
+# Exercise 1
+
+array = [1, 2, 3, 4, 5]
+array.push(6).shift
+
+# Exercise 2
+
+[1, 3, 5, 4, 2].sort
+
+# Exercise 3 
+
+array = [1, 3, 5, 4, 2]
+array.sort!.push(6).shift 
+
+# Exercise 4
+
+[1, 2, 3, 4, 5].sum
+
+# Exercise 5 
+[1, 2, 3, 4, 5].sum * 2
+
--- first-pair-session/spec/arrays_spec.rb
+++
@@ -1,5 +0,0 @@
-describe 'Arrays' do
-    it 'returns [2, 3, 4, 5, 6]'
-    expect(arrays([1, 2, 3, 4, 5])).to eq '[2, 3, 4, 5, 6]'
-    end
-end
